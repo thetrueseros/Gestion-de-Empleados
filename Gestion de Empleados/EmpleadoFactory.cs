edebb8478ed2c71717674961a9bef93ef360302e
thetrueseros <leugimdzn@hotmail.com>
2025-10-24 13:57:17 -0500

    Creación clase EmpleadoFactory (Factory Method)

---
+++ Gestion de Empleados/EmpleadoFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_Empleados
+{
+    public class EmpleadoFactory
+    {
+        public void CrearEmpleado(string tipo, double salarioBase, double adicional)
+        {
+            Empleado empleado = null;
+            switch (tipo.ToLower())
+            {
+                case "gerente":
+                    empleado = new Gerente(salarioBase, adicional);
+                    break;
+                case "programador":
+                    empleado = new Programador(salarioBase, adicional);
+                    break;
+                default:
+                    throw new ArgumentException("Tipo de empleado no reconocido.");
+            }
+            Console.WriteLine($"Salario total del {tipo}: {empleado.CalcularSalario()}");
+        }
+    }
+}
