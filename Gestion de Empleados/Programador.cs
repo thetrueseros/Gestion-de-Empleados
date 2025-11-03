f1c47d03297d909daa17797e7ab4825ff2169e5c
thetrueseros <leugimdzn@hotmail.com>
2025-11-03 14:40:50 -0500

    Validación en clase Programador

--- Gestion de Empleados/Programador.cs
+++ Gestion de Empleados/Programador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Gestion_de_Empleados
 {
@@ -16,7 +17,16 @@ namespace Gestion_de_Empleados
         }
         public override double CalcularSalario()
         {
-            return SalarioBase + (HorasExtra * 20); // Suponiendo que cada hora extra vale 20 unidades monetarias
+            if (SalarioBase > 2500000)
+            {
+                MessageBox.Show("El salario base excede el límite para pago de horas extra.");
+                return SalarioBase; // No se pagan horas extra si el salario base es mayor a 2,500,000
+            }
+            else
+            {
+                return SalarioBase + (HorasExtra * 20); // Suponiendo que cada hora extra vale 20 unidades monetarias
+            }
+                
         }
     }
 }
